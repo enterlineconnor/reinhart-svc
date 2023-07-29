ecc73e5822f30b907fba9c692d41158677de8ed9
Connor Enterline <connor@freemarkethealth.com>
2023-07-29 01:02:47 -0400

    create new gym endpoint

--- api/model/gym.py
+++ api/model/gym.py
@@ -8,6 +8,7 @@ class GymPayload(BaseModel):
     gym_name: str
     subdomain: str
 
+
 def create_gym(new_gym: GymPayload, conn):
     """Get all user data"""
 
@@ -21,8 +22,5 @@ def create_gym(new_gym: GymPayload, conn):
         ) 
     """
     cursor = conn.cursor()
-    cursor.execute(
-        sql,
-        new_gym.gym_name,
-        new_gym.subdomain
-    )
+    cursor.execute(sql, new_gym.gym_name, new_gym.subdomain)
+    conn.commit()
--- api/router/gym.py
+++ api/router/gym.py
@@ -1,13 +1,14 @@
 """Requests related to gym based processes"""
 
 from fastapi import APIRouter, Depends
-from ..model.gym import create_gym
+from ..model.gym import GymPayload, create_gym
 from ..database.utils.uow import get_connection
+
 router = APIRouter()
 
 
 @router.post("")
-async def create_new_gym(conn = Depends(get_connection)):
+async def create_new_gym(new_gym: GymPayload, conn=Depends(get_connection)):
     """GET all base data associated with the user."""
-    create_gym(conn)
+    create_gym(new_gym, conn)
     return {"message": "success"}
